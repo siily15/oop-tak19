406546eff6aced82c2dfd6da283305a3383290e4
siily <arnokosk15@gmail.com>
2021-04-15 13:11:41 +0300

    task edit

--- controllers/TasksController.php
+++ controllers/TasksController.php
@@ -1,23 +1,58 @@
 <?php
 
-class TasksController {
+class TasksController
+{
 
-    public function index () {
+    public function index()
+    {
 
         global $app;
 
         $tasks = $app['database']->selectAll('todos');
 
         return view('tasks', ['tasks' => $tasks]);
-
     }
-    public function store () {
+
+    public function store()
+    {
 
         global $app;
 
         $app['database']->insert('todos', [
             'description' => $_POST['description'],
         ]);
+
+        return redirect('tasks');
+    }
+
+    public function edit()
+    {
+
+        global $app;
+
+        $task = $app['database']->selectById('todos', $_GET['id']);
+
+        return view('edit-task', ['task' => $task]);
+    }
+
+    public function update()
+    {
+
+        global $app;
+
+        $app['database']->update('todos', $_POST['id'], [
+            'description' => $_POST['description'],
+        ]);
+
+        return redirect('tasks');
+    }
+
+    public function delete()
+    {
+
+        global $app;
+
+        $app['database']->delete('todos', $_GET['id']);
 
         return redirect('tasks');
     }
